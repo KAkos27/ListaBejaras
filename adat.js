396523aeb6312000c8d4ff59e7a9e1e48dedc95d
KAkos27 <akoskosztolanyi@gmail.com>
2024-04-04 09:37:12 +0200

    ListaBejaras

---
+++ adat.js
@@ -0,0 +1,29 @@
+const emberekLista = [
+  {
+    nev: "Éva",
+    kor: 12,
+    nem: true,
+  },
+  {
+    nev: "Ádám",
+    kor: 22,
+    nem: false,
+  },
+  {
+    nev: "Jenő",
+    kor: 32,
+    nem: false,
+  },
+  {
+    nev: "Liza",
+    kor: 23,
+    nem: true,
+  },
+  {
+    nev: "Kati",
+    kor: 19,
+    nem: true,
+  },
+];
+
+export default emberekLista;
---
+++ main.js
@@ -0,0 +1,40 @@
+import emberekLista from "./adat.js";
+
+for (let index = 0; index < emberekLista.length; index++) {
+  const elem = emberekLista[index];
+  console.log(elem);
+}
+
+emberekLista.forEach((ember, i) => {
+  console.log(ember, i);
+});
+
+const ferfiak = emberekLista.filter((ember) => {
+  return ember.nem === false;
+});
+
+console.log(ferfiak);
+
+const fiatalok = emberekLista.filter((ember) => {
+  return ember.kor < 20;
+});
+
+console.log(fiatalok);
+
+const oregFerfiak = emberekLista.filter((ember) => {
+  return ember.kor > 20 && !ember.nem;
+});
+
+console.log(oregFerfiak);
+
+emberekLista.sort((a, b) => {
+  return b.kor - a.kor;
+});
+
+console.log(emberekLista);
+
+emberekLista.sort((a, b) => {
+  return Math.random() * 10 - 5;
+});
+
+console.log(emberekLista);
